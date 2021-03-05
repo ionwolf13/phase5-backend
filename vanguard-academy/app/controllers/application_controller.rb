76114a33767a9835f359b00272f0cf2f69be749d
jesus_garcia <voli.scorp13@gmail.com>
2021-03-05 17:55:39 -0600

    full crud for student and stu_class

--- vanguard-academy/app/controllers/application_controller.rb
+++ vanguard-academy/app/controllers/application_controller.rb
@@ -1,16 +1,55 @@
 class ApplicationController < ActionController::API
 
-    def LoggedIn
-        !!current_user
-    end
+#     before_action :authorized
+   
+#     def encode_token(payload)
+#       # this "secret password" should be unique to this project and MUST MATCH where it is used in the decode
+#       # later on in this controller in the decoded_token method
+#       JWT.encode(payload, "ChildrenOfTheCode")
+#     end
+    
+#     # Helper function to check a request if there is an Authorization header
+#     def auth_header
+#       request.headers['Authorization']
+#     end
 
-    def current_user
-        User.find([session[:id]])
-    end
+#     # Check to see if this request has the auth header, and if the token passed in 
+#     # is authenticated by the JWT decoding authentication
+#     def decoded_token
+#       if auth_header
+#         # label token from the header of the request, the first word will be Bearer by convention
+#         # If you send in JUST the token as your Authorization you won't need the split
+#         token = auth_header.split(' ')[1]
+#         begin
+#           # decode the token with your secret password/phrase
+#           # This sequence is important to have the true and, for now, this algorithm
+#           #   You can look into what they mean on your own, but just know they help JWT stuff work.
+#           JWT.decode(token, "ChildrenOfTheCode", true, algorithm: 'HS256')
+#         rescue JWT::DecodeError
+#           nil
+#         end
+#       end
+#     end
 
-    def not_logged_in
-        render json: {error: "Need to Sign In!"}
-    end
+#     # Used to find the current user if their token passes it's authentication
+#     def current_user
+#       if decoded_token
+#         # Using the decoded token, grab the user_id stored within it and find a user
+#         user_id = decoded_token[0]['user_id']
+#         @user = User.find_by(id: user_id)
+#       end
+#     end
+
+#     # Check if a user is logged in through a series of chained methods
+#     def logged_in?
+#       !!current_user
+#     end
+
+#     # If a user is NOT logged in, it sends the message "Please log in"
+#     def authorized
+#       render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+#     end
+#   end
 
 
 
--- vanguard-academy/app/controllers/sessions_controller.rb
+++ vanguard-academy/app/controllers/sessions_controller.rb
@@ -1,10 +1,6 @@
 class SessionsController < ApplicationController
 
     
-    def new
-
-    end
-
     def create
         
         user = User.find_by(username: params[:username])
@@ -18,17 +14,23 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             session[:loggedIn] = true
             session[:current_role] = "student"
-            render json: user.to_json({include: [:rooms => {:include => [:instructor]}], except: [:created_at, :updated_at]})
+            render json: user.to_json({include: [:rooms => {:include => [:instructor => {:include => [:assignments => {:include => [:student_assignments]}]}]}], except: [:created_at, :updated_at]})
         else
-            render json: {error: "Inalid Username/Password", status: 505}
+            render json: {message: "Inalid Username/Password"}, status: unauthorized
         end
-
+        # .to_json({})
     end
 
     def destroy
         session[:user_id] = nil
         session[:loggedIn] = false
         render json: {user: {}, message: "Logout Complete"}
+    end
+
+    private
+
+    def login_params
+        params.require(:user).permit(:username,:password)
     end
 
 
