93ad1af890bc12c475fecf57f13df030fca0e9a5
jesus_garcia <voli.scorp13@gmail.com>
2021-03-07 17:11:23 -0600

    need to fix student assignment crud

--- vanguard-academy/config/routes.rb
+++ vanguard-academy/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, :rooms, :student_rooms, :instructors
+  resources :users, :rooms, :student_rooms, :instructors, :student_assignments
   resources :schools
 
   # get '/login', to: 'sesssions#new'
